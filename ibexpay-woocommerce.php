c89ab11c8151c3b8524bd6fa0dd1f9e832888219
Roberto Solares <betosolaresgar@gmail.com>
2022-05-09 13:23:43 -0600

    Add basic process payment

--- ibexpay-woocommerce.php
+++ ibexpay-woocommerce.php
@@ -22,11 +22,14 @@ function init_ibexpay_woocommerce() {
             $this->has_fields = false;
             $this->method_title = 'IBEXPay WooCommerce';
             $this->method_description = 'The easiest and fastest way for any business to receive instant Bitcoin payments via the Lightning Network.';
+
             $this->init_form_fields();
             $this->init_settings();
+
             $this->enabled = $this->get_option('enabled');
             $this->title = $this->get_option('title');
             $this->description = $this->get_option('description');
+            $this->button_id = $this->get_option('button_id');
 
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
         }
@@ -52,8 +55,40 @@ function init_ibexpay_woocommerce() {
                     'title' => __('Description', 'woocommerce'),
                     'type' => 'textarea',
                     'description' => __('The payment method description which a customer sees at the checkout of your store.', 'woocommerce'),
-                    'default' => __('The easiest and fastest way for any business to receive instant Bitcoin payments via the Lightning Network.')
+                    'default' => __('Pay with Bitcoin over the Lightning Network. Powered by IBEX')
                 ),
+
+                'button_id' => array(
+                    'title' => __('Button ID', 'woocommerce'),
+                    'type' => 'text',
+                    'description' => __('Button ID from IBEXPay', 'woocommerce'),
+                    'default' => __('')
+                ),
+            );
+        }
+
+        public function process_payment($order_id) {
+            $order = wc_get_order($order_id);
+            $order_items = $order->get_items();
+            $callback = trailingslashit(get_bloginfo('wpurl')) . '?wc-api=wc_ibexpay_woocommerce';
+            $success = add_query_arg('order', $order->get_id(), add_query_arg('key', $order->get_order_key(), $this->get_return_url($order)));
+            $description = '';
+
+            foreach($order_items as $item_id => $item) {
+                $item_data = $item->get_data();
+                $description .= $item_data['name'] . ' (X' . $item_data['quantity'] . '), ';
+            }
+
+            $description = substr($description, 0, -2);
+
+            $ibexpay_order = 'order123';
+            $redirect = 'https://ibexpay.ibexmercado.com/checkout/' . $ibexpay_order;
+
+            WC()->cart->empty_cart();
+
+            return array(
+                'result'   => 'success',
+                'redirect' => $redirect,
             );
         }
     }
